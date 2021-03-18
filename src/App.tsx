b14e39e9f9efe96bb73a091022660250fa2ec3d2
maxwellpeck <maxwell.peck@uvm.edu>
2021-03-17 21:59:02 -0400

    more typescript and eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css';
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import './App.css';
 
 const App = (): JSX.Element => {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './App.css';
+
+const Home = (): JSX.Element => {
+    return (
+        <div className="App">
+            <p>Welcome to my site</p>
+        </div>
+    );
+};
+
+export default Home;
