04dd6a9ade69c739316ae6949174d64bd6c7bdd0
maxwellpeck <maxwell.peck@uvm.edu>
2021-03-21 09:43:01 -0400

    testing autodeploy with cloudfront dist/route 53 aliases/new cert for root and subdomains

--- src/components/Vegometer/Vegometer.tsx
+++ src/components/Vegometer/Vegometer.tsx
@@ -5,6 +5,7 @@ export const Vegometer = (): JSX.Element => {
     return (
         <div className="Vegometer">
             <p>A Ridiculous iOS App</p>
+            <p>Hi there d</p>
         </div>
     );
 };
