3d79c9ba209b4c87a51a96e2284b4086a8297fe8
maxwellpeck <maxwell.peck@uvm.edu>
2021-04-02 07:27:18 -0400

    Added app store link to vegometer page

--- src/components/Vegometer/Vegometer.tsx
+++ src/components/Vegometer/Vegometer.tsx
@@ -3,16 +3,21 @@ import screenshot from './screenshot.png';
 import './Vegometer.css';
 
 export const Vegometer = (): JSX.Element => {
+    const vegometerURL = "https://apps.apple.com/us/app/veg-o-meter/id1560019489";
+    
     return (
         <div className="Vegometer">
             <div className="flex-container">
                 <div className="left">
-                    <img className="screenshotImg" src={screenshot} alt="Screenshot" />
+                    <a href={vegometerURL}>
+                        <img className="screenshotImg" src={screenshot} alt="Screenshot" />
+                    </a>
                 </div>
                 <div className="right">
                     <h1 className="title">Veg-O-Meter</h1>
                     <p className="subtitle">A Ridiculous iOS App</p>
                     <p>Everyone needs time to veg out!  How hard are you vegging?  Find out today with the Veg-O-Meter!</p>
+                    <a className="veg-link" href={vegometerURL}>Available in the App Store {'>>'}</a>
                     <div className="privacy-policy">
                         <h2 className="title">Privacy Policy</h2>
                         <p className="privacy-policy-paragraph">
